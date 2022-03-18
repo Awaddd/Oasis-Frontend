2b2db7af68027b8304d857709cbf9a89d6a4bb1f
Awad <34748292+Awaddd@users.noreply.github.com>
2022-03-18 09:42:46 +0000

    Refactor middleware

--- src/utils/types/global.ts
+++ src/utils/types/global.ts
@@ -1,4 +1,11 @@
 import { ReactNode } from "react";
+import type { NextFetchEvent } from "next/server";
+import type { NextRequest } from "next/server";
+
+export type Middleware = (
+  request: NextRequest,
+  event: NextFetchEvent
+) => Promise<Response | undefined> | Response | undefined;
 
 export type FooterProps = {
   dark?: boolean;
