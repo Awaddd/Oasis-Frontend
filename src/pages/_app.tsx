b3661bc1bec7ce8f58ec27f35ef410bf4c448d6f
Awad <34748292+Awaddd@users.noreply.github.com>
2022-03-18 09:56:40 +0000

    Sidebar now closes on route change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,35 @@
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, useRecoilState } from 'recoil';
+import { sidebarIsOpenState } from '../state/state';
 
 import '../styles/main.css';
 
+const App = ({ component }: { component: any }) => {
+  const [, setSidebarIsOpen] = useRecoilState(sidebarIsOpenState);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => setSidebarIsOpen(false);
+    router.events.on('routeChangeStart', handleRouteChange);
+    return () => router.events.off('routeChangeStart', handleRouteChange);
+  }, []);
+
+  return component;
+};
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
+
+  const component = <Component {...pageProps} />;
 
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-        <Component {...pageProps} />
+        <App component={component} />
       </RecoilRoot>
     </QueryClientProvider>
   )
