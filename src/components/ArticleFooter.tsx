e55be7904ab887a5492155de422336cc70846de4
Awad <34748292+Awaddd@users.noreply.github.com>
2022-03-20 17:31:24 +0000

    update email svg

--- src/components/ArticleFooter.tsx
+++ src/components/ArticleFooter.tsx
@@ -9,7 +9,7 @@ const ArticleFooter: FC<Props> = ({ color }) => (
   <footer className={`flex flex-col text-center footer py-[24px] md:py-lg md:pb-[45px] px-lg ${color}`}>
     <h4 className="font-bold text-gray-900">Thanks for reading</h4>
     <p className="text-sm">Make sure to check back for weekly updates!</p>
-    <div className="flex self-center mt-[16px]">
+    <div className="flex self-center items-center mt-[16px">
       <SocialIconBar dark={true} />
     </div>
   </footer>
