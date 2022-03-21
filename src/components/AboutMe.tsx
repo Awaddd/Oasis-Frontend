df2203d8ca0fb884232754609c8f67b1dae2acc0
Awad <34748292+Awaddd@users.noreply.github.com>
2022-03-21 21:22:44 +0000

    Added author picture and update design

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { api } from "../services/api";
 import { AuthorBio } from "../utils/types/global";
 import SocialIconBar from "./SocialIconBar";
 
@@ -8,25 +9,29 @@ type Props = {
 
 const AboutMe: FC<Props> = ({ data }) => {
   if (!data) return null;
-  const { firstName, lastName, bio, email, socialLinks } = data;
+  const { firstName, lastName, bio, email, picture, socialLinks } = data;
 
   return (
     <main className="bg-gray-900 global-padding py-lg md:py-xl">
       <div className="container mx-auto 2xl:w-9/12">
-        <div className="items-center lg:flex">
-          <article className="lg:w-1/2 lg:px-xl md:px-[45px]">
+        <div className="items-center md:flex md:px-[45px] md:gap-8 lg:px-0 lg:gap-0">
+          <article className="lg:px-xl md:w-3/4 lg:w-max">
             <h2 className="text-3xl font-bold text-gray-100">{firstName} {lastName}</h2>
 
             {bio && (<p className="text-gray-400 lg:max-w-md">{bio}</p>)}
 
-            <div className="flex items-center mt-6 -mx-2">
+            <div className="items-center hidden mt-6 -mx-2 md:flex lg:mt-2">
               <SocialIconBar links={socialLinks} email={email} />
             </div>
           </article>
 
-          <div className="mt-8 lg:mt-0 lg:w-1/2">
-            <div className="flex items-center justify-end h-64 bg-gray-800 rounded-md">
-            </div>
+          <div className="mt-8 sm:w-64 md:w-32 md:self-start lg:self-center lg:w-48 lg:mt-0 lg:ml-[-24px]">
+            <img src={`${api}${picture?.url}`} alt="author image" className="object-contain rounded-lg md:rounded-full" />
+          </div>
+
+
+          <div className="flex items-center mt-4 -mx-2 md:hidden lg:mt-2">
+            <SocialIconBar links={socialLinks} email={email} />
           </div>
         </div>
       </div>
