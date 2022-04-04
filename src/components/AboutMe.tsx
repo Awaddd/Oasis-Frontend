a4374231bb591e9e91f4ff37b35fbc669f7dbd8a
Awad <34748292+Awaddd@users.noreply.github.com>
2022-04-05 00:54:52 +0100

    Fix author image

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -15,7 +15,7 @@ const AboutMe: FC<Props> = ({ data }) => {
     <main className="bg-gray-900 global-padding py-lg md:py-xl">
       <div className="container grid mx-auto 2xl:w-9/12">
         <div className="items-center md:flex md:px-[45px] md:gap-8 lg:px-0 lg:gap-0 justify-self-center">
-          <article className="lg:pr-xl md:w-3/4 lg:w-max">
+          <article className="lg:pr-xl md:w-8/12 lg:w-max">
             <h2 className="text-3xl font-bold text-gray-100">{firstName} {lastName}</h2>
 
             {bio && (<p className="text-gray-400 lg:max-w-md">{bio}</p>)}
@@ -25,8 +25,8 @@ const AboutMe: FC<Props> = ({ data }) => {
             </div>
           </article>
 
-          <div className="mt-8 sm:w-64 md:w-32 md:self-start lg:self-center lg:w-48 lg:mt-0 lg:ml-[-24px]">
-            {picture && (<img src={`${api}${picture?.url}`} alt="author image" className="object-contain rounded-lg md:rounded-full" />)}
+          <div className="mt-8 md:mt-0 sm:w-64 md:w-[10rem] md:h-[10rem] md:self-start lg:self-center lg:w-[10rem] lg:h-[10rem] lg:mt-0 lg:ml-[-24px]">
+            {picture && (<img src={`${api}${picture?.url}`} alt="author image" className="object-fill w-full h-full rounded-lg shadow-inner md:rounded-full" />)}
           </div>
 
           <div className="flex items-center mt-4 -mx-2 md:hidden lg:mt-2">
