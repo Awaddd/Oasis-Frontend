0d9848c4b0034400cd752d1687c984cdcd099b0c
Awad <34748292+Awaddd@users.noreply.github.com>
2022-04-05 13:57:45 +0100

    Testing on demand ISR

--- src/pages/api/rebuild.ts
+++ src/pages/api/rebuild.ts
@@ -1,6 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+type ApiResponse = NextApiResponse & {
+  unstable_revalidate: (path: string) => void;
+};
+
+export default async (req: NextApiRequest, res: ApiResponse) => {
   console.log("req", req);
-  return res.status(200).json({ message: "test" });
+
+  try {
+    await res.unstable_revalidate("/books");
+    await res.unstable_revalidate("/book");
+
+    return res.status(200).json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).json({ message: "Error validating" });
+  }
 };
