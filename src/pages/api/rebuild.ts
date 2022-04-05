41fc0d02fc6e781a76d74b3b3fb304277455c11f
Awad <34748292+Awaddd@users.noreply.github.com>
2022-04-05 13:10:42 +0100

    Add logs

--- src/pages/api/rebuild.ts
+++ src/pages/api/rebuild.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default (_: NextApiRequest, res: NextApiResponse) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("req", req);
   return res.status(200).json({ message: "test" });
 };
