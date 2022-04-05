0ee8cbac8d5cd669816f00f4f1d886e22475048e
Awad <34748292+Awaddd@users.noreply.github.com>
2022-04-05 13:07:42 +0100

    Fix build

--- src/pages/api/rebuild.ts
+++ src/pages/api/rebuild.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default (_: NextApiRequest, res: NextApiResponse) => {
   return res.status(200).json({ message: "test" });
 };
