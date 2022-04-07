899203e74c531c8caf35388dd3d2663bb1713917
Awad <34748292+Awaddd@users.noreply.github.com>
2022-04-07 12:26:20 +0100

    Brought back author image on mobile

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -27,8 +27,8 @@ const AboutMe: FC<Props> = ({ data, imageProps }) => {
           </article>
 
           {imageProps && (
-            <figure className="relative mt-8 md:mt-0 sm:w-64 md:w-[10rem] md:h-[10rem] md:self-start lg:self-center lg:w-[10rem] lg:h-[10rem] lg:mt-0 lg:ml-[-24px]">
-              <Image layout="fill" {...imageProps} placeholder="blur" priority alt="author image" className="object-fill rounded-lg shadow-inner md:rounded-full" />
+            <figure className="relative mt-8 md:mt-0 h-[20rem] sm:w-64 md:w-[10rem] md:h-[10rem] md:self-start lg:self-center lg:w-[10rem] lg:h-[10rem] lg:mt-0 lg:ml-[-24px]">
+              <Image layout="fill" {...imageProps} placeholder="blur" priority alt="author image" className="object-fill h-full rounded-lg shadow-inner md:rounded-full" />
             </figure>
           )}
 
