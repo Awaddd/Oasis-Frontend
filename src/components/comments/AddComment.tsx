c83c2bf095a78455b77d33bd110e716b11f578db
Awad, Dini <awad.dini@sky.uk>
2022-09-22 11:00:45 +0100

    Updated add comment ui

--- src/components/comments/AddComment.tsx
+++ src/components/comments/AddComment.tsx
@@ -1,8 +1,8 @@
 const AddComment = () => {
   return (
     <form className="flex flex-col gap-[0.4rem]">
-      <textarea className="w-full p-2 border-2 border-black rounded" />
-      <button className="self-end btn">Post</button>
+      <textarea className="w-full p-2 border border-gray-300 rounded" rows={4} />
+      <button className="btn-flex md:self-end md:mt-[0.1rem] min-h-8 px-8 py-[0.4rem] font-medium">Add Comment</button>
     </form>
   );
 };
