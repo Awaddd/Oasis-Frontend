b0870632d0a57bc29ebe17049dc856ad16de3471
Awad, Dini <awad.dini@sky.uk>
2022-09-23 14:38:30 +0100

    Moved middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
-import type { Middleware as MiddlewareType } from "../utils/types/global";
+import type { Middleware as MiddlewareType } from "./utils/types/global";
 
 const Middleware: MiddlewareType = (req) => {
-  if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
+  if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase()) {
     return NextResponse.next();
+  }
   return NextResponse.redirect(
     `${req.nextUrl.origin}${req.nextUrl.pathname.toLowerCase()}`
   );
